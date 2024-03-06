2b760a8bada0fc2af614606599fd956136e2c9d7
benjamindpb <benjamin.dpb@gmail.com>
2024-03-06 03:08:09 -0400

    feat: Map component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,10 @@
-import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiYmVuamFtaW5kcGIiLCJhIjoiY2x0ZmJ4a2NsMG9xbTJwbjlyeTJzdnhscyJ9.3Hcg4PS1aq8o12OLrhdooQ';
+import React from 'react';
+import Map from './components/Map';
 
 export default function App() {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-70.6465);
-  const [lat, setLat] = useState(-33.4946);
-  const [zoom, setZoom] = useState(9);
-
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/satellite-v9',
-      center: [lng, lat],
-      zoom: zoom
-    });
-
-    map.current.on('move', () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-  });
-
   return (
     <div>
-      <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </div>
-      <div ref={mapContainer} className="map-container" />
+      <Map />
     </div>
   );
 }
