89ce68700d9f273a795c342803da4b35afb3bd1c
benjamindpb <benjamin.dpb@gmail.com>
2024-03-07 04:38:33 -0400

    fix: remove map sidebar

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYmVuamFtaW5kcGIiLCJhIjoiY2x0ZmJ4a2NsMG9xbTJwbjlyeTJzdnhscyJ9.3Hcg4PS1aq8o12OLrhdooQ';
@@ -6,31 +6,19 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYmVuamFtaW5kcGIiLCJhIjoiY2x0ZmJ4a2NsMG9xbTJwb
 const Map = () => {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(-70.6465);
-  const [lat, setLat] = useState(-33.4946);
-  const [zoom, setZoom] = useState(9);
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: 'mapbox://styles/mapbox/satellite-v9',
-      center: [lng, lat],
-      zoom: zoom
-    });
-    
-    map.current.on('move', () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
+      center: [-70.6465, -33.4946],
+      zoom: 9,
     });
   });
 
   return (
-    <div>
-      <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </div>
+    <div className='mt-3'>
       <div ref={mapContainer} className="map-container" />
     </div>
   );
